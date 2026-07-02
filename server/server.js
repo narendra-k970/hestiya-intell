e885d812c90e6f97fac4f805d1d05afd115ef209
narendra-k970 <75464307+narendra-k970@users.noreply.github.com>
2026-07-02 13:51:47 +0530

    5173

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
+      "http://localhost:5173",
       "https://marketplace.hestiya.com",
       "https://intelligence.hestiya.com",
     ],
